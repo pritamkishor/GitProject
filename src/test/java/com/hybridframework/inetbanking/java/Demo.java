b9fe63c50231f7eb5e957d789731e241bc5dbd39
PRITAM <charmprit69@gmail.com>
2022-05-17 03:19:23 +0530

    This is newly designed prj module

---
+++ src/test/java/com/hybridframework/inetbanking/java/Demo.java
@@ -0,0 +1,16 @@
+package com.hybridframework.inetbanking.java;
+
+public class Demo {
+
+	public static void main()
+	{
+		System.out.println("Hi all this is my new project");
+		System.out.println("Hi all");
+		System.out.println("Hi all");
+		System.out.println("Hi all this is my new project");
+		System.out.println("Hi all");System.out.println("Hi all this is my new project");
+		System.out.println("Hi all");
+	
+	}
+	
+}
